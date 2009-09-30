c6dd001c1a95763b2ea62201b73005a6b86c048e
Simon Rozet <simon@rozet.name>
2009-09-30 15:10:44 +0200

    Test SCM#dir_for instead of private #path method

--- test/git_test.rb
+++ test/git_test.rb
@@ -11,15 +11,16 @@ class BobGitTest < Test::Unit::TestCase
   end
 
   def path(uri, branch="master")
-    SCM::Git.new(uri, branch).__send__(:path)
+    SCM::Git.new(uri, branch).dir_for("commit").
+      sub(Bob.directory, "").to_s[1..-1]
   end
 
   def test_converts_repo_uri_into_path
-    assert_equal "git-github-com-integrity-bob-master",
+    assert_equal "git-github-com-integrity-bob-master/commit",
       path("git://github.com/integrity/bob")
-    assert_equal "git-example-org-foo-repo-master",
+    assert_equal "git-example-org-foo-repo-master/commit",
       path("git@example.org:~foo/repo")
-    assert_equal "tmp-repo-git-master", path("/tmp/repo.git")
-    assert_equal "tmp-repo-git-foo",    path("/tmp/repo.git", "foo")
+    assert_equal "tmp-repo-git-master/commit", path("/tmp/repo.git")
+    assert_equal "tmp-repo-git-foo/commit",    path("/tmp/repo.git", "foo")
   end
 end
--- test/svn_test.rb
+++ test/svn_test.rb
@@ -11,20 +11,21 @@ class BobSvnTest < Test::Unit::TestCase
   end
 
   def path(uri)
-    SCM::Svn.new(uri, "").__send__(:path)
+    SCM::Svn.new(uri, "").dir_for("commit").
+      sub(Bob.directory, "").to_s[1..-1]
   end
 
   test "converts svn repo uri into a path" do
-    assert_equal "http-rubygems-rubyforge-org-svn",
+    assert_equal "http-rubygems-rubyforge-org-svn/commit",
       path("http://rubygems.rubyforge.org/svn/")
 
-    assert_equal "svn-rubyforge-org-var-svn-rubygems",
+    assert_equal "svn-rubyforge-org-var-svn-rubygems/commit",
       path("svn://rubyforge.org/var/svn/rubygems")
 
-    assert_equal "svn-ssh-developername-rubyforge-org-var-svn-rubygems",
+    assert_equal "svn-ssh-developername-rubyforge-org-var-svn-rubygems/commit",
       path("svn+ssh://developername@rubyforge.org/var/svn/rubygems")
 
-    assert_equal "home-user-code-repo",
+    assert_equal "home-user-code-repo/commit",
       path("/home/user/code/repo")
   end
 end
